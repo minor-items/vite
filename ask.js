4036c1c050ecea8f7d341c9bf5f995ee2a8a8283
liuyi <liuyi@gennlife.com>
2022-01-05 19:29:43 +0800

    -

--- ask.js
+++ ask.js
@@ -1,17 +1,24 @@
 module.exports = [
   {
-    name: "a",
+    name: "aprojectName",
     type: "input",
-    description: "参数一",
+    description: "document title (项目默认标题)",
   },
   {
-    name: "b",
-    type: "input",
-    description: "参数二",
-  },
-  {
-    name: "c",
-    type: "input",
-    description: "参数三",
+    type: 'confirm',
+    name: 'isVuex',
+    message: 'iDo you need vuex (是否需要vuex)',
+    default: false
   },
 ];
+
+// {
+//   name: "package",
+//   type: "list",
+//   description: "参数二",
+//   choices: [
+//     { name: '小米', value: 'mi' },
+//     { name: '华为', value: 'huawei' },
+//     { name: '苹果', value: 'apple' }
+//   ]
+// }
